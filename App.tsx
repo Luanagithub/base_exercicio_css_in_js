5c9fc2867cd0ab85a03ad8c059a945a0b7803e68
Luanagithub <122471005+Luanagithub@users.noreply.github.com>
2023-09-20 04:44:56 -0300

    Fix prettier

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import Header from './components/Cabecalho'
+import Vagaz from './Estilos'
+import Hero from './components/Hero'
+import ListaVagas from './containers/ListaVagas'
+import { GlobalStyle } from './Global'
+
+import './global.css'
+
+function App() {
+  return (
+    <>
+    <GlobalStyle>  </GlobalStyle>
+
+      <Header>
+        <h1>EBAC Jobs</h1>
+      </Header>
+      <Hero />
+
+      <div className="container">
+        <Vagaz>
+          <ListaVagas />
+        </Vagaz>
+      </div>
+    </>
+  )
+}
+
+export default App
